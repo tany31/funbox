073be0c072544f55b0c33ad8532ecae9c6d2b11b
Tanya Syrtsova <syrtsova.tanya@gmail.com>
2018-11-12 12:59:17 +0700

    Updates

---
+++ precache-manifest.e81aaa36bded145a9c9c30a471200833.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "38a2d03d3fb82e201659616f54385567",
+    "url": "/funbox/static/media/funbox_logo.38a2d03d.png"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/funbox/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "be8b6e8627b36d0925a5",
+    "url": "/funbox/static/js/runtime~main.be8b6e86.js"
+  },
+  {
+    "revision": "7e04a6dab33f8b57be86",
+    "url": "/funbox/static/js/main.7e04a6da.chunk.js"
+  },
+  {
+    "revision": "5941505055fbc6aef3e3",
+    "url": "/funbox/static/js/1.59415050.chunk.js"
+  },
+  {
+    "revision": "7e04a6dab33f8b57be86",
+    "url": "/funbox/static/css/main.f8d5ded5.chunk.css"
+  },
+  {
+    "revision": "0b26666f7a0e724b39df5561d70baad6",
+    "url": "/funbox/index.html"
+  }
+];
